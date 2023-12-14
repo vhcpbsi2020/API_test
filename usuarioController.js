9799fe968794c9d8ab3b8724f33e89fad7aa50f9
Administrador <administrador@qriarlabs-victo>
2023-12-14 14:47:27 -0300

    first commit2

---
+++ usuarioController.js
@@ -0,0 +1,23 @@
+const UsuarioService = require('../services/usuarioService');
+
+const usuarioService = new UsuarioService()
+class UsuarioController {
+   static  async cadastrar(req, res) {
+        const { nome, email, senha } = req.body
+
+        try {
+            const usuario = await usuarioService.cadastrar({ nome, email, senha })
+
+            res.status(201).send(usuario)
+
+        } catch (error) {
+            res.status(400).send({messagem: error.message})
+
+        }
+
+
+    }
+
+}
+
+module.exports = UsuarioController
